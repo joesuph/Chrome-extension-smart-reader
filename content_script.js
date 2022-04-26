ac998840fa5c2695e67496fa4e23c70be8dd3945
Joseph <joseph.olsen98@yahoo.com>
2022-04-26 12:16:29 -0700

    Changed from twitter read aloud to general read aloud of nodes with similar parents

--- content_script.js
+++ content_script.js
@@ -1,25 +1,102 @@
-var tweets = document.getElementsByClassName('tweet-text');
-var names = document.querySelectorAll('.FullNameGroup');
-var index = 0;
-var started = false;
-
 var u = new SpeechSynthesisUtterance();
 u.lang = 'en-US';
 u.rate = 1.2;
+
+/*
 document.body.onmouseup = ()=>{
-    if(!started){
-        started = true;
-        startSpeech();
+        speakList(getSimilarToSelected());
+}
+*/
+
+function speakList(list){
+    console.log(u)
+    var index = 0;
+
+    u.onend = (event)=>{
+        if(++index<list.length)
+            speak_text(list[index])
+    }
+    var speak_text = (text)=>{
+        u.text = text;
+        speechSynthesis.speak(u);
+    }
+
+    speak_text(list[0])
+
+}
+
+function speak_text(text){
+    u.text = text;
+    speechSynthesis.speak(u);
+}
+
+
+function getSelectedNode()
+{
+    if (document.selection)
+        return document.selection.createRange().parentElement();
+    else
+    {
+        var selection = window.getSelection();
+        if (selection.rangeCount > 0)
+            return selection.getRangeAt(0).startContainer.parentNode;
     }
 }
 
-u.onend = function(event) { 
-    if(++index<tweets.length)   
-        startSpeech();
+function getParentAndClassList(node){
+    var list = [];
+    var classList = [node.classList];
+    var parentList = [node.tagName];
+    
+    var currentNode = node;
+
+    while (currentNode.parentElement.tagName != "BODY")
+    {
+        currentNode = currentNode.parentElement;
+        parentList.push(currentNode.tagName)
+        classList.push(currentNode.classList)
+    } 
+
+    list.push(parentList)
+    list.push(classList)
+    return list;
 }
 
-function startSpeech()
-{
-    u.text = names[index].innerText.substr(0,names[index].innerText.length-4) + " Said " + tweets[index].innerText; 
-    speechSynthesis.speak(u);
+function ancestorListToNodes(list){
+
+    var children = null;
+    var currentNode = document.body;
+
+    var recurseSearch = (node,i)=>{
+        if (i == -1)
+            return [node.innerText];        
+        
+        var result = [];
+        if (node.children)
+
+        for (var c=0;c<node.children.length;c++){
+
+            if (/*node.children[c].classList == list[1][i]
+                && */node.children[c].tagName == list[0][i])
+                {
+                   var returnVal = recurseSearch(node.children[c],i-1) 
+
+                   if (returnVal.length)
+                    result.push(...returnVal);
+                }     
+        }
+        return result;
+
+    }
+
+    return recurseSearch(currentNode,list[0].length-1);
+}
+
+function getSimilarToSelected(){
+    var node = getSelectedNode()
+    var parent_list = getParentAndClassList(node);
+    var result = ancestorListToNodes(parent_list)
+    console.log(result)
+
+    return result
 }
--- control_script.js
+++ control_script.js
@@ -1,1 +1,14 @@
 chrome.tabs.executeScript({file: 'content_script.js'});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var start = document.getElementById('start');
+    var stop = document.getElementById('stop');
+
+    // onClick's logic below:
+    start.addEventListener('click', function() {
+        chrome.tabs.executeScript( null, {code:"speakList(getSimilarToSelected());"})
+    });
+    stop.addEventListener('click', function() {
+        chrome.tabs.executeScript( null, {code:"speechSynthesis.cancel()"})
+    });
+});
